aa6f29d9305b0e2f6eecd864093bcb46fbe93a47
Akshat Shrivastava <134301995+ak-s-h-at@users.noreply.github.com>
2023-06-07 02:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,111 @@
 import csv
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import scrolledtext
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+import numpy as np
+from sklearn.metrics import pairwise_distances
+from tkinter import Tk, Label, Entry, Button, Listbox, END
 
 
-dataset_path = r'D:\Task 2- Movie Recommendation system\movies.csv'
-with open(dataset_path, 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    next(reader)  # Skip header row
-    movie_data = list(reader)
+movies_dataset_path = r'D:\Task 2- Movie Recommendation system\movies.csv'
+ratings_dataset_path = r'D:\Task 2- Movie Recommendation system\rating.csv'
 
-movies = []
-genres = []
-for movie in movie_data:
-    movie_id, title, genre = movie
-    genres.append(genre.split('|'))
-    movies.append(title)
+def load_movie_ratings(filename):
+    movie_ratings = {}
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        next(reader)  
+        for row in reader:
+            user_id, movie_id, rating = int(row[0]), int(row[1]), float(row[2])
+            if user_id not in movie_ratings:
+                movie_ratings[user_id] = {}
+            movie_ratings[user_id][movie_id] = rating
+    return movie_ratings
 
-genres_list = [' '.join(g) for g in genres]
-tfidf = TfidfVectorizer()
-tfidf_matrix = tfidf.fit_transform(genres_list)
+def load_movie_names(filename):
+    movie_names = {}
+    with open(filename, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        next(reader) 
+        for row in reader:
+            movie_id, movie_name = int(row[0]), row[1]
+            movie_names[movie_id] = movie_name
+    return movie_names
 
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-window = tk.Tk()
+movie_ratings = load_movie_ratings(ratings_dataset_path)
+movie_names = load_movie_names(movies_dataset_path)
+
+
+num_users = max(movie_ratings.keys()) + 1
+num_movies = max(max(user_ratings.keys()) for user_ratings in movie_ratings.values()) + 1
+
+ratings_matrix = np.zeros((num_users, num_movies))
+for user_id, user_ratings in movie_ratings.items():
+    for movie_id, rating in user_ratings.items():
+        ratings_matrix[user_id, movie_id] = rating
+
+def calculate_similarity_matrix():
+    global similarity_matrix
+    similarity_matrix = 1 - pairwise_distances(ratings_matrix, metric="cosine")
+
+calculate_similarity_matrix()
+
+def make_recommendations(user_ratings, ratings_matrix):
+    user_id = num_users - 1
+    user_ratings = movie_ratings[user_id]
+
+    similarity_scores = similarity_matrix[user_id]
+    weighted_scores = np.dot(similarity_scores, ratings_matrix)
+    sorted_indices = np.argsort(weighted_scores)[::-1]
+    recommendations = []
+    for index in sorted_indices:
+        if index not in user_ratings:
+            movie_id = index
+            movie_name = movie_names[movie_id]
+            recommendations.append(movie_name)
+        if len(recommendations) == 5:
+            break
+    return recommendations
+
+def rate_movie():
+    movie_name = movie_entry.get()
+    rating = float(rating_entry.get())
+    if movie_name in movie_names.values():
+        movie_id = list(movie_names.keys())[list(movie_names.values()).index(movie_name)]
+        user_ratings[movie_id] = rating
+        movie_entry.delete(0, END)
+        rating_entry.delete(0, END)
+        ratings_matrix[num_users - 1, movie_id] = rating
+        calculate_similarity_matrix()
+        recommended_movies = make_recommendations(user_ratings, ratings_matrix)
+        display_recommended_movies(recommended_movies)
+
+def display_recommended_movies(recommended_movies):
+    window = Tk()
+    window.title("Recommended Movies")
+    window.geometry("400x400")
+    movie_listbox = Listbox(window)
+    movie_listbox.pack()
+    for movie in recommended_movies:
+        movie_listbox.insert(END, movie)
+    window.mainloop()
+
+window = Tk()
 window.title("Movie Recommendation System")
-window.geometry("400x300")
-
-label = tk.Label(window, text="Enter a movie title:")
-label.pack()
-entry = tk.Entry(window)
-entry.pack()
+window.geometry("400x400")
 
 
-recommendation_text = scrolledtext.ScrolledText(window, height=10)
-recommendation_text.pack()
-
-def recommend_movies():
-    movie_title = entry.get().strip()
-    if movie_title:
-        movie_index = -1
-        for index, movie in enumerate(movies):
-            if movie_title.lower() in movie.lower():
-                movie_index = index
-                break
-        
-        if movie_index != -1:
-            similarity_scores = list(enumerate(cosine_similarities[movie_index]))
-            similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-            similar_movies_indices = [i[0] for i in similarity_scores]
-            recommended_movies = [movies[i] for i in similar_movies_indices if i != movie_index]
-            recommendation_text.delete("1.0", tk.END)
-            for movie in recommended_movies[:10]:
-                recommendation_text.insert(tk.END, f"- {movie}\n")
-        else:
-            messagebox.showwarning("Warning", "Movie not found in the dataset.")
-    else:
-        messagebox.showwarning("Warning", "Please enter a movie title.")
+movie_label = Label(window, text="Enter Movie Name:")
+movie_label.pack()
+movie_entry = Entry(window)
+movie_entry.pack()
 
 
-button = tk.Button(window, text="Get Recommendations", command=recommend_movies)
-button.pack()
+rating_label = Label(window, text="Enter Rating (1-5):")
+rating_label.pack()
+rating_entry = Entry(window)
+rating_entry.pack()
+
+
+rate_button = Button(window, text="Rate Movie", command=rate_movie)
+rate_button.pack()
 
 window.mainloop()
